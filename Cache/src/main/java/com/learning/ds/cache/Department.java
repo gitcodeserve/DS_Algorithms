4af73e0c77ca6991d1a19ed6840aead9d11772a6
Shaan <shantanu.sahay@gmail.com>
2023-02-27 23:24:41 +0530

    LRU Cache code using LinkedHaspMap

--- Cache/src/main/java/com/learning/ds/cache/Department.java
+++ Cache/src/main/java/com/learning/ds/cache/Department.java
@@ -1,2 +1,14 @@
-package com.learning.ds.cache;public class Department {
+package com.learning.ds.cache;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    private int deptId;
+    private String deptName;
 }
--- Cache/src/main/java/com/learning/ds/cache/Employee.java
+++ Cache/src/main/java/com/learning/ds/cache/Employee.java
@@ -1,2 +1,16 @@
-package com.learning.ds.cache;public class Employee {
+package com.learning.ds.cache;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private int empId;
+    private String empName;
+
 }
